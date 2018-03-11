9c3a83fa749aae4fcf49a3812f9884d602c20965
Chusovliankin Aleksei <lixich@mail.ru>
2018-03-11 23:53:56 +0500

    fix metrics

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 #!flask/bin/python
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 import requests
 
 app = Flask(__name__)
 
 url_set = ['http://lixichcska.pythonanywhere.com/', 'http://lixichamkar.pythonanywhere.com/']
-las_seconds = 60
+last_seconds = 1.0
 
 @app.route('/<path:path>', methods = ['GET', 'PUT', 'POST', 'DELETE'])
 def home(path):
-    min_url = url_set[0]
-    min_perf = requests.get(min_url + 'metrics/' + str(las_seconds)).json()['AverageResponseTime']
-    for url in url_set[1:]:
-        perf = requests.get(url + 'metrics/' + str(las_seconds)).json()['AverageResponseTime']
-        if float(perf) < min_perf:
-            min_url = url
-            min_perf = perf
+    min_url = ''
+    min_perf = 10000
+    for url in url_set:
+        try:
+            perf = requests.get(url + 'metrics/' + str(last_seconds)).json()['AverageResponseTime']
+            if float(perf) < min_perf:
+                min_url = url
+                min_perf = perf
+        except:
+            pass
+    if min_url == '':
+        print('No servers available')
+        return make_response(jsonify({'Error': 'No servers available'}), 404)
     print(str(min_url+path), request.method, request.json)
     if request.method =='GET':
         return jsonify(requests.get(min_url + path).json())
@@ -28,4 +34,4 @@ def home(path):
         return jsonify(requests.post(min_url + path, json=request.json).json())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True, debug=True)
